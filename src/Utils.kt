4e307754e428957d5f2592d7d68fb3aed70b253b
ttkalcevic <tkalcevic.tomislav@gmail.com>
2021-12-01 13:38:31 +0100

    [Day01] Solution

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,7 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+
+fun List<Int>.countIncreases(window: Int = 1): Int = this.windowed(window) { it.sum() }
+    .zipWithNext().count { (prev, next) -> next > prev }
